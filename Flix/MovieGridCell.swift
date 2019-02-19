29d28e87a6b699f08aaac2775b542089621b792f
Hailey Cassidy <HaileyCassidy@wireless-10-146-243-182.public.utexas.edu>
2019-02-18 18:20:51 -0600

    flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Hailey Cassidy on 2/17/19.
+//  Copyright © 2019 Hailey Cassidy. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
